4818d0207483b413fabb62325af2b67d388f25a5
Robert Wiśniowski <robertjwisniowski@gmail.com>
2023-09-10 16:41:40 +0200

    Routing was added.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello there2';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('login', 'DefaultController');
+Router::get('cars', 'DefaultController');
+Router::get('booking', 'DefaultController');
+Router::run($path);
